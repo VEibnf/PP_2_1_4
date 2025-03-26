171e52705fe6060bae44b2ea083e1382123859c1
VEibnf <Ve.infobiz@gmail.com>
2025-03-26 23:25:18 +0700

    2.1.4 ready

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,6 @@
 package koschei.models;
 
 public class Deth8 {
+    public Deth8(Deth8 deth) {
+    }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
+    public Rabbit4(Duck5 duck) {
+    }
+
     @Override
     public String toString() {
         return ", в зайце утка " + "";
